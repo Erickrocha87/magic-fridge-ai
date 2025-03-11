84f6d511925b5e85e1e8621c2d773142cfad76f9
erickrocha87 <ericksaggi31@gmail.com>
2025-03-11 19:40:27 -0300

    refactor: implements Spring AI, fist contact with Spring AI

--- src/main/java/com/rocha/MagicFridgeAi/controllers/RecipeController.java
+++ src/main/java/com/rocha/MagicFridgeAi/controllers/RecipeController.java
@@ -4,11 +4,9 @@ import com.rocha.MagicFridgeAi.entity.Food;
 import com.rocha.MagicFridgeAi.services.ChatGptService;
 import com.rocha.MagicFridgeAi.services.FoodService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -20,10 +18,9 @@ public class RecipeController {
     private  final FoodService foodService;
     private final ChatGptService chatGptService;
 
-    @GetMapping("/generate")
-    public Mono<ResponseEntity<String>> generateRecipe(){
+    @GetMapping("/informations")
+    public String generateRecipe(){
         List<Food> foods = foodService.findAll();
-        return chatGptService.generateRecipe(foods)
-                .map(ResponseEntity::ok).defaultIfEmpty(ResponseEntity.noContent().build());
+        return chatGptService.generateRecipe(foods);
     }
 }
--- src/main/java/com/rocha/MagicFridgeAi/services/ChatGptService.java
+++ src/main/java/com/rocha/MagicFridgeAi/services/ChatGptService.java
@@ -2,51 +2,27 @@ package com.rocha.MagicFridgeAi.services;
 
 import com.rocha.MagicFridgeAi.entity.Food;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ChatGptService {
 
-    private final WebClient webClient;
-    private String apiKey = System.getenv("API_KEY");
+    private final OpenAiChatModel chatClient;
 
-    public Mono<String> generateRecipe(List<Food> foodsRequest) {
 
+
+    public String generateRecipe(List<Food> foodsRequest){
         String foods = foodsRequest.stream()
                 .map(food -> String.format("%s (%s) - Quantity: %d, Validate: %s",
                         food.getName(), food.getCategory(), food.getQuantity(), food.getValidate()))
                 .collect(Collectors.joining("\n"));
 
         String prompt = "Based on the following foods, suggest a creative and practical recipe" + foods;
-        Map<String, Object> requestBody = Map.of(
-                "model", "gpt-3.5-turbo",
-                "messages", List.of(
-                        Map.of("role", "system", "content", "You are a kitchen assistant who creates recipes"),
-                        Map.of("role", "user", "content", prompt)
-                )
-        );
-        return webClient.post()
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + apiKey)
-                .bodyValue(requestBody)
-                .retrieve()
-                .bodyToMono(Map.class)
-                .map(response -> {
-                    var choices = (List<Map<String, Object>>) response.get("choices");
-                    if (choices != null && !choices.isEmpty()) {
-                        Map<String, Object> message = (Map<String,Object> ) choices.get(0).get("message");
-                        return message.get("content").toString();
-                    }
-                   return "Any receive was generated";
-                });
+        return chatClient.call(prompt);
     }
 }
